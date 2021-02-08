114d3bae99f9493e6a7334984602e35e7e81a9d1
Velirax <74984085+Velirax@users.noreply.github.com>
2021-02-08 20:27:17 +0200

    Changes
    Changes

---
+++ chill.js
@@ -0,0 +1,7 @@
+
+module.exports = { chill: function(){
+
+    const playlist = 'https://www.youtube.com/watch?v=einR_rqDICU&ab_channel=YordanAngelov';
+
+    return playlist;
+}};
---
+++ info.js
@@ -0,0 +1,14 @@
+
+module.exports = { info: function(){
+
+    const commands = 'Supported Commands:\n'+
+    '-Dani - Days since last ban\n' + 
+    '-tilted - Article for avoiding tilt \n' + 
+    '-cringe - C R I N G E (Top 5 An*mes)\n' +
+    '-chill - TOP VIBES\n' +
+    '-stream @username - PUSKAI STREAM WE\n' +
+    '-rank1 vlad - RANK1 VLAD HEAVY 🧢 \n' +
+    '-mm - Check if you are in Losers or Winners Queue(using Algebra 2 and algorithms from NASA)';
+
+    return commands;
+}};
