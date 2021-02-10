22bee5f8d5291d5a7e64030abce4b72fd2619ccc
Damian Ivanov <37500930+damianivanov@users.noreply.github.com>
2021-02-10 19:15:52 +0200

    consoleLog

--- timeDani.js
+++ timeDani.js
@@ -3,7 +3,8 @@ module.exports  = { timeOfday: function()
         var currentTime = new Date();
         var hours = currentTime.getHours();
         var day = currentTime.getUTCDay();
-        if ((day >= 1  && day <= 5) && (hours >= 8 && hours <= 16))
+        console.log(`days: ${day} - hours:${hours}`);
+        if ((day >= 1  && day <= 5) && (hours >= 8 && hours <= 17))
             return "Dani is currently in school getting this 🍆"
         else
             return "Dani is waiting for new year to get this 🍆"
